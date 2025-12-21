f28664a234c17549e297fce9e9b5df96aa8e998d
Muhammad <faisalkhan969681@gmail.com>
2025-12-21 18:58:52 +0500

    added taskroute and taskcontroller

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,14 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
+<<<<<<< HEAD
 const connectDB= require("./config/db");
+=======
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+>>>>>>> 0abd4c3 (taskRoutes and taskController done)
 
 const app = express();
 
@@ -24,10 +31,17 @@ connectDB();
 app.use(express.json());
 
 // Routes
+<<<<<<< HEAD
 // app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
+=======
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/reports", reportRoutes);
+>>>>>>> 0abd4c3 (taskRoutes and taskController done)
 
 
 // Start Server
