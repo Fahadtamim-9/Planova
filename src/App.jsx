6f00fb0d02b1a9c9eefa96043cf1af1a35ff23f2
Fahadtamim-9 <mfahadtamim04@gamil.com>
2025-12-28 15:46:24 +0500

    Added new features to our project planova

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Admin/Dashboard";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import ManageTasks from "./pages/Admin/ManageTasks";
 import CreateTask from "./pages/Admin/CreateTask";
 import ManageUsers from "./pages/Admin/ManageUsers";
+
 import UserDashboard from "./pages/User/UserDashboard";
 import MyTasks from "./pages/User/MyTasks";
 import ViewTaskdetails from "./pages/User/ViewTaskdetails";
+
 import PrivateRoute from "./pages/routes/PrivateRoute";
+import UserProvider, { UserContext } from "./context/userContext";
 
 const App = () => {
   return (
+     <UserProvider>
     <div>
       <Router>
         <Routes>
@@ -33,10 +37,27 @@ const App = () => {
             <Route path="/user/tasks" element={<MyTasks />} /> 
             <Route path="/user/task-details/:id" element={<ViewTaskdetails />} />
           </Route>
+
+          {/* < default Route */}
+          <Route path="/" element={<Root />} />
+
         </Routes>
       </Router>
     </div>
+    </UserProvider>
   );
 };
 
-export default App  
+export default App 
+
+const Root = () => {
+  const { user, loading } = useContext(UserContext);
+
+  if (loading) return <Outlet />;
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />;
+}; 
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,5 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../Components/layouts/AuthLayout'
+import { Link, useNavigate } from 'react-router-dom';
+import ProfilePhotoSelector from '../../Components/Inputs/Profilephotoselector';
+import Input from '../../Components/Inputs/Inputs';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
 
 const Signup = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -10,10 +17,15 @@ const Signup = () => {
 
   const [error, setError] = useState(null) ;
 
+  const {updateUser} = useContext(UserContext) 
+    const navigate = useNavigate();
+
    // Handle SingUp Form Submit 
     const HandleSignUp= async (e) => {
       e.preventDefault() ;
-    };
+  
+let profileImageUrl = '' 
+
  
     if (!fullName) {
       setError("Please enter full name.");
@@ -34,7 +46,44 @@ if (!ValidEmail(email)) {
     setError("");
 
     //SignUp API Call
-  };
+    try {
+
+      // upload image if present 
+      if (profilePic) {
+      const imgUploadRes = await uploadImage (profilePic)
+      profileImageUrl = imgUploadRes.imgUrl || "";
+    } 
+
+  const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+    name: fullName,
+    email,
+    password,
+      profileImageUrl,
+    adminInviteToken
+  });
+
+  const { token, role } = response.data;
+
+  if (token) {
+    localStorage.setItem("token", token);
+    updateUser(response.data);
+
+    //Redirect based on role
+if (role === "admin") {
+  navigate("/admin/dashboard");
+} else {
+  navigate("/user/dashboard");
+}
+
+   } 
+  } catch (error) {
+  if (error.response && error.response.data.message) {
+    setError(error.response.data.message);
+  } else {
+    setError("Something went wrong. Please try again.");
+  }
+}
+    };
 
 
   return (
@@ -48,14 +97,62 @@ if (!ValidEmail(email)) {
         <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
          
          <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >
-          </div> 
-          </form>
+          <Input
+             value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            /> 
+
+
+  <Input
+        value= {email}
+        onChange= {({target}) => setEmail(target.value) }
+        label= "Email Adress"
+        placeholder= "john@xample.com"
+        type="text"
+        />
+     
+      <Input
+        value= {password}
+        onChange= {({target}) => setPassword(target.value) }
+        label= "Password"
+        placeholder= "Min 8 Characters"
+        type="password"
+       />
+
+       <Input
+        value= {adminInviteToken}
+        onChange= {({target}) => setAdminInviteToken(target.value) }
+        label= "Admin Invite Token"
+        placeholder= "6 Digit code "
+        type="text"
+       />
+         </div> 
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p> }
+
+        <button type="submit" className="btn-primary">
+          SIGN UP 
+          </button>
+
+          <p className="text-[13px]  text-slate-800 mt-3">
+            Already have an account? {" "}
+            <Link className="font-medium text-primary underline" to="/login">
+            Login 
+            </Link>
+            </p> 
+
+        
+          </form> 
 
       </div>
 
 
     </AuthLayout>
-  )
+  ) 
+};
 
 
 export default Signup
